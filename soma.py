eefc3810c85a1910be68323758d74ca05d19f06f
Beatriz França Gusmão <beatrizgusmao421@outlook.com>
2020-09-29 22:02:38 -0300

    Calculadora básica com if

---
+++ soma.py
@@ -0,0 +1,15 @@
+a = input ("Qual o primeiro valor?")
+b = input ("Qual o se gundo valor?")
+operacao = input ("Qual a operação?")
+if operacao == "+":
+    resultado = a + b
+    print (resultado)
+else if operacao == "-": 
+    resultado = a - b
+    print (resultado)
+else if operacao == "*":
+    resultado = a * b
+    print (resultado)
+else: 
+    resultado = a / b
+    print (resultado)
